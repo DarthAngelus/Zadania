0111e7dd4d9233fbb3f211ba78a33e1cd0076d87
Darth_Angel <tamtamtamtamtamtamtamtaramtam@xsxs.pl>
2021-07-03 10:31:18 +0200

    v.1

--- Zadanie_01/Program.cs
+++ Zadanie_01/Program.cs
@@ -10,37 +10,72 @@ namespace Zadanie_01
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("KALKULATOR");
+            Console.WriteLine("================");
+            Console.WriteLine(">> KALKULATOR <<");
+            Console.WriteLine("================\n");
+            do
+                Glowna();
+            while (Koniec());
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Metoda do zakończenia aplikacji jeśli wciśnięty ostanie znak "n"
+        /// </summary>
+        /// <returns>Podanie "n" zwraca false by wyjść z pętli w metodzie Main </returns>
+        private static bool Koniec()
+        {
+            Console.Write("\nCzy chcesz kontynuować? [T/N]: ");
+            char end = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.WriteLine();
+            if (end == 'n' || end == 'N')
+            {
+                Console.WriteLine("==============");
+                Console.WriteLine("|   Koniec   |");
+                Console.WriteLine("==============");
+                return false;
+            }
+            else return true;
+        }
+        /// <summary>
+        /// Pobieranie zmiennych od użytkownika i wywołanie metody Operator
+        /// </summary>
+        static void Glowna()
+        { 
             Console.Write("Pierwsza liczba:\t");
             string s = Get();
             float a = Convert.ToSingle(s);
-            //s = s.Replace(".", ",");
+            
             Console.Write("Operator (+,-,*,/):\t");
             char oper = Console.ReadKey().KeyChar;
-            Console.Write("\nDruga liczba:\t");
+            
+            Console.Write("\nI druga liczba:\t\t");
             s = Get();
-
-            //Console.WriteLine(Convert.ToSingle(s));
             float b = Convert.ToSingle(s);
 
-            Console.WriteLine(a + " | " + b);
+            Console.WriteLine("\tOperacja: " + a + " " + oper + " " + b);
 
-            //string s = Get();
             Operator(oper, a, b);
-
-            Console.ReadKey();
         }
+        /// <summary>
+        /// zmiana ewentualnej "." na "," w stringu będącym w założeniu liczbą,
+        /// aby nie było problemu przy konwersji do float w przypadku podania w innym formacie
+        /// </summary>
+        /// <returns></returns>
         static string Get()
         {
             return Console.ReadLine().Replace(".",",");
         }
+        /// <summary>
+        /// Wybór odpowiedniego działana matem. w zależności od pobranej oper z metody Glowna
+        /// </summary>
+        /// <param name="znak"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
         static void Operator(char znak, float a, float b)
         {
-            //Console.WriteLine("Operator (+,-,*,/): ");
             //char znak = Convert.ToChar(Console.ReadKey());
             //char znak = Console.ReadKey().KeyChar;
-           // float wynik;
             switch (znak)
             {
                 case (char)'+':
@@ -53,29 +88,59 @@ namespace Zadanie_01
                     Pisz(Pomnoz(a, b).ToString());
                     break;
                 case (char)'/':
-                    Console.WriteLine();
+                    if (b != 0.0)
+                        Pisz(Podziel(a, b).ToString());
+                    else
+                        Console.WriteLine("Pamiętaj ty jeden, nie dziel przez zero!");
+                    break;
+                default:
+                    Console.WriteLine("Nieobsługiwany operator");
                     break;
             }
         }
+        /// <summary>
+        /// Wyświetla tekst + stringa
+        /// </summary>
+        /// <param name="v"></param>
         private static void Pisz(string v)
         {
             //string s;
             Console.WriteLine("Wynik działania:\t" + v);
         }
-
+        /// <summary>
+        /// Operacja dodawania
+        /// </summary>
+        /// <returns>obliczony wynik działania</returns>
         private static float Dodaj(float a, float b)
         {
             float wynik = a + b;
             return wynik;
         }
+        /// <summary>
+        /// Operacja odejmowania
+        /// </summary>
+        /// <returns>obliczony wynik działania</returns>
         private static float Odejmij(float a, float b)
         {
             float wynik = a - b;
             return wynik;
         }
+        /// <summary>
+        /// Operacja mnożenia
+        /// </summary>
+        /// <returns>obliczony wynik działania</returns>
         private static float Pomnoz(float a, float b)
         {
             float wynik = a * b;
+            return wynik;
+        }
+        /// <summary>
+        /// Operacja dzielenia
+        /// </summary>
+        /// <returns>obliczony wynik działania</returns>
+        private static float Podziel(float a, float b)
+        {
+            float wynik = a / b;
             return wynik;
         }
     }
